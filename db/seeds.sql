3c00d51a5ae15509840ab1e53080504e722c18a1
CruzAzul <no-reply>
2019-09-21 18:56:13 -0400

    server, connection and models set-up

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Burger Boy', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Burger Time', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Buffalo Burger', true);
+
